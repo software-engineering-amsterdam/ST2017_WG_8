c5a0b95c0ba05749c45468d90f65670b89a480c3
icemonster <nunossabino@hotmail.com>
2017-09-07 11:59:18 +0200

    Added my solution to the problem9 of Project Euler

---
+++ nuno/exercise9.hs
@@ -0,0 +1,10 @@
+module Lab1 where
+import Data.List
+import Test.QuickCheck  
+
+answers = [a*b*c | a <- [0..1000], b <- [a+1..1000], c <- [b+1..1000], a+b+c == 1000, a^2 + b^2 == c^2]
+
+-- Running this in interactive Haskell might take a few seconds
+main = print (head (answers))
+
+--time: 30 minutes
