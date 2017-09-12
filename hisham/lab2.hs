2fe320abe8676f4906105846fe80ca1800aed7f1
Metchu <ahmednassir@hotmail.com>
2017-09-12 14:16:12 +0200

    added assignment3 to lab2

--- hisham/lab2.hs
+++ hisham/lab2.hs
@@ -10,6 +10,9 @@ infix 1 -->
 (-->) :: Bool -> Bool -> Bool
 p --> q = (not p) || q
 
+forall :: [a] -> (a -> Bool) -> Bool
+forall = flip all
+
 probs :: Int -> IO [Float]
 probs 0 = return []
 probs n = do
@@ -62,3 +65,25 @@ isIsosceles a b c = a==b || a==c || b==c
 --
 isRectangular :: Integer -> Integer -> Integer -> Bool
 isRectangular a b c = a^2+b^2==c^2 || a^2+c^2==b^2 || b^2+c^2==a^2
+
+
+--Assignment 3: Time: 2 hours
+stronger, weaker :: [a] -> (a -> Bool) -> (a -> Bool) -> Bool
+stronger xs p q = forall xs (\ x -> p x --> q x)
+weaker   xs p q = stronger xs q p
+
+--(\ x -> even x && x > 3) even
+--(\ x -> even x || x > 3) even
+--(\ x -> (even x && x > 3) || even x) even
+--even (\ x -> (even x && x > 3) || even x)
+firstProp :: Int -> Bool
+firstProp x = even x && x > 3
+
+secondProp :: Int -> Bool
+secondProp x = even x || x > 3
+
+thirdProp :: Int -> Bool
+thirdProp x = (even x && x > 3) || even x
+
+fourthProp :: Int -> Bool
+fourthProp x = mod x 2 == 0
