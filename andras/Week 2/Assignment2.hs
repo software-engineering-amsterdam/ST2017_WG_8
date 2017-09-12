97be9147f9b888063e062e1b6e8669299c629c53
Herczeg <h.herczeg.andras@gmail.com>
2017-09-12 13:56:48 +0200

    Updated solutions

--- andras/Week 2/Assignment2.hs
+++ andras/Week 2/Assignment2.hs
@@ -32,9 +32,9 @@ checkRandom x = [(length(filter(<=0.25) x)), (length(filter(\x -> x > 0.25 && x 
 Result 1:
 main :: IO()
 main = do
-			x <- probs 10000
-			-- We assume that if a container has more/less than 2500 +- (2500 * 0.05) the distribution is not random enough
-			print(if length(filter (\x -> x >= 2375 && x <= 2625) (checkRandom x)) == 4 then "Random distribution checks out" else "Distribution is not random")
+            x <- probs 10000
+            -- We assume that if a container has more/less than 2500 +- (2500 * 0.05) the distribution is not random enough
+            print(if length(filter (\x -> x >= 2375 && x <= 2625) (checkRandom x)) == 4 then "Random distribution checks out" else "Distribution is not random")
 -}
 
 
@@ -141,11 +141,83 @@ Weak
 
 
 -----------------------------------------------------------------------------------------
--- 4. Time spent: ~ 20 minutes
+-- 4. Time spent: ~ 40 minutes
+-- TODO: Properties, tests
 
 isPermutation :: Eq a => [a] -> [a] -> Bool
-isPermutation [x] [y] = [x] elem (subsequences [y]) 
+isPermutation [] [] = True
+isPermutation x y = (length [x] == length [y]) && (all (\z -> z `elem` y) x) 
 
 
+-- main = print(isPermutation [1, 2, 3] [3, 1, 2])
 
-main = checkBatch3
+
+-----------------------------------------------------------------------------------------
+-- 5. Time spent: ~ 40 minutes
+-- TODO: Properties, QuickCheck
+
+isDerangement :: Eq a => [a] -> [a] -> Bool
+isDerangement [] [] = False
+isDerangement x y = (isPermutation x y) && parsePermutation x y
+
+parsePermutation :: Eq a => [a] -> [a] -> Bool
+parsePermutation [] [] = True
+parsePermutation x y = if (head x == head y ) then False else parsePermutation (tail(x)) (tail(y)) 
+
+
+deran :: Eq a => [a] -> [[a]]
+deran [] = []
+deran x = filter (\y -> isDerangement x y) (permutations x)
+
+-- Result 5:
+-- main = print(isDerangement <list> <list>)
+-- main = print(deran <list>)
+
+-----------------------------------------------------------------------------------------
+-- 6. Time spent: ~ 30 minutes
+-- TODO: Specifications, quickCheck
+
+-- To turn a char into its ROT13 equivalent I just map it from one list to the other
+-- The '#' at the end of both list is for invalid (non-alphabetical) chars
+normalAlph = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#"
+rot13Alph  = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm#"
+
+-- If any char of the input string is not a letter, we replace it with '#'
+sanitizeString :: String -> String
+sanitizeString s = [(if notElem c normalAlph then '#' else c) | c <- s]
+
+-- Turn a char into its ROT13 equivalent
+findRot :: Char -> Char
+findRot c = rot13Alph!!(head(elemIndices c normalAlph))
+
+-- Transform each char of the input String
+sentanceToRot :: String -> String
+sentanceToRot s = [findRot c | c <- s]
+
+-- Result 6:
+-- main = print(sentanceToRot(sanitizeString <String>))
+
+-----------------------------------------------------------------------------------------
+-- 7. Time spent: ~ 50 minutes
+
+alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+-- Step 1: Move the first 4 chars to the end
+move4 :: String -> String
+move4 s = (drop 4 s) ++ (take 4 s)
+
+-- Step 2: Convert chars into integers (A -> 10, B -> 11, .. , Z -> 35)
+convertLetters :: String -> String
+convertLetters s = concat[maybeLetter c | c <- s]
+
+maybeLetter :: Char -> String
+maybeLetter c | elem c alph = show(((\(Just i)->i)(elemIndex c alph)) + 10)
+              | otherwise = [c] 
+
+-- Step 3: Check mod 97 == 1
+iban :: String -> Bool
+iban s = (length s <= 34) && (read(convertLetters(move4(s))) `mod` 97 == 1)
+
+validIbans = ["AL47212110090000000235698741", "AD1200012030200359100100", "AT611904300234573201", "AZ21NABZ00000000137010001944", "BH67BMAG00001299123456", "BE62510007547061", "BA391290079401028494", "BG80BNBG96611020345678", "HR1210010051863000160", "CY17002001280000001200527600", "CZ6508000000192000145399", "DK5000400440116243", "EE382200221020145685", "FO9754320388899944", "FI2112345600000785", "FR1420041010050500013M02606", "GE29NB0000000101904917", "DE89370400440532013000", "GI75NWBK000000007099453", "GR1601101250000000012300695", "GL5604449876543210", "HU42117730161111101800000000", "IS140159260076545510730339", "IE29AIBK93115212345678", "IL620108000000099999999", "IT40S0542811101000000123456"]
+
+main = print (all (iban) validIbans)
