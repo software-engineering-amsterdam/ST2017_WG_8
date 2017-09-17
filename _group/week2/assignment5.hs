c72cc7aa83bb4be4da8b0d0d29f852545dd39811
Pepijn van Vlaanderen <pepijn@vanvlaanderen.biz>
2017-09-17 12:42:40 +0200

    Added ex. 5 and updated ex. 5 in group.

--- _group/week2/assignment5.hs
+++ _group/week2/assignment5.hs
@@ -5,55 +5,42 @@ import Data.Char
 import System.Random
 import Test.QuickCheck
 
--- 4. Time spent: ~ 1 hour
-
-infix 1 --> 
+infix 1 -->
 
 (-->) :: Bool -> Bool -> Bool
 p --> q = (not p) || q
 
+{-
+Exercise 5 ~ 2 hours
+-}
 isDerangement :: Eq a => [a] -> [a] -> Bool
-isDerangement [] [] = False
-isDerangement x y = (isPermutation x y) && parsePermutation x y
+isDerangement xs ys = isPermutation xs ys && and (zipWith (/=) xs ys)
 
-parsePermutation :: Eq a => [a] -> [a] -> Bool
-parsePermutation [] [] = True
-parsePermutation x y = if (head x == head y ) then False else parsePermutation (tail(x)) (tail(y)) 
+-- Create a list with derangement lists
+deranList :: Eq a => [a] -> [[a]]
+deranList xs = filter (\ys -> isDerangement xs ys) (permutations xs)
 
-
-deran :: Eq a => [a] -> [[a]]
-deran [] = []
-deran x = filter (\y -> isDerangement x y) (permutations x)
-
+-- Properties
 -- A list can't be its own derangement
-property51 :: [Int] -> [Int] -> Bool
-property51 x y = isDerangement x y --> not(x == y)
+prop51 :: Ord a => [a] -> [a] -> Bool
+prop51 [] [] = True
+prop51 xs ys = isDerangement xs ys --> not(xs == ys)
 
 -- Derangement is symmetric, if A is a derangement of B then B is a derangement of A
-property52 :: [Int] -> [Int] -> Bool
-property52 x y = isDerangement x y --> isDerangement y x
+prop52 :: Ord a => [a] -> [a] -> Bool
+prop52 xs ys = isDerangement xs ys --> isDerangement ys xs
 
+-- Derangement preservers the length
+prop53 :: Ord a => [a] -> [a] -> Bool
+prop53 xs ys = isDerangement xs ys --> length xs == length ys
 
-testR2 :: Int -> Int -> ([Int] -> [Int] -> Bool) -> IO ()
-testR2 k n f = if k == n then print (show n ++ " tests passed")
-                else do
-                  x <- genIntList 20 10
-                  y <- genIntList 20 10
-                  if (f x y) then
-                    testR2 (k+1) n f
-                  else error ("failed: " ++ show x ++ show y)
+-- Derangement is a permutation
+prop54 :: Ord a => [a] -> [a] -> Bool
+prop54 xs ys = isDerangement xs ys --> isPermutation xs ys
 
-checkBatch5 :: IO()
-checkBatch5 = do
-    print("Testing property 1")
-    testR2 0 100 property51
-    print("Testing property 2")
-    testR2 0 100 property52
-
-
-
+-- Combine all props for a simple quickCheck
+checkProps5 :: Ord a => [a] -> [a] -> Bool
+checkProps5 xs ys = and [prop51 xs ys, prop52 xs ys, prop53 xs ys, prop54 xs ys]
 
 -- Result 5:
--- main = print(isDerangement <list> <list>)
--- main = print(deran <list>)
--- main = checkBatch5
+-- main = quickCheck checkProps5
