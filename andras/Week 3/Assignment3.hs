03417df46e10d977016943090601275cada02bd8
Herczeg <h.herczeg.andras@gmail.com>
2017-09-21 00:57:15 +0200

    updated solution 1, 2 and 3

--- andras/Week 3/Assignment3.hs
+++ andras/Week 3/Assignment3.hs
@@ -1,160 +1,152 @@
-import Data.Char
 import Data.List
 import System.Random
 import Test.QuickCheck
+import System.Random
+import Lecture3
 
-type Name = Int
-
-data Form = Prop Name
- | Neg Form
- | Cnj [Form]
- | Dsj [Form]
- | Impl Form Form
- | Equiv Form Form
- deriving (Eq,Ord)
-
-instance Show Form where
- show (Prop x) = show x
- show (Neg f) = '-' : show f
- show (Cnj fs) = "*(" ++ showLst fs ++ ")"
- show (Dsj fs) = "+(" ++ showLst fs ++ ")"
- show (Impl f1 f2) = "(" ++ show f1 ++ "==>" ++ show f2 ++ ")"
- show (Equiv f1 f2) = "(" ++ show f1 ++ "<=>" ++ show f2 ++ ")"
-
-propNames :: Form -> [Name]
-propNames = sort.nub.pnames where
- pnames (Prop name) = [name]
- pnames (Neg f) = pnames f
- pnames (Cnj fs) = concatMap pnames fs
- pnames (Dsj fs) = concatMap pnames fs
- pnames (Impl f1 f2) = concatMap pnames [f1,f2]
- pnames (Equiv f1 f2) = concatMap pnames [f1,f2]
-
-allVals :: Form -> [Valuation]
-allVals = genVals . propNames
-
-type Valuation = [(Name,Bool)]
-genVals :: [Name] -> [Valuation]
-genVals [] = [[]]
-genVals (name:names) =
- map ((name,True) :) (genVals names)
- ++ map ((name,False):) (genVals names)
-
-
-
-showLst,showRest :: [Form] -> String
-showLst [] = ""
-showLst (f:fs) = show f ++ showRest fs
-showRest [] = ""
-showRest (f:fs) = ' ': show f ++ showRest fs
-
-
-
-data Token = TokenNeg
- | TokenCnj
- | TokenDsj
- | TokenImpl
- | TokenEquiv
- | TokenInt Int
- | TokenOP
- | TokenCP
- deriving (Show,Eq)
-
-lexer :: String -> [Token]
-lexer [] = []
-lexer (c:cs) | isSpace c = lexer cs
- | isDigit c = lexNum (c:cs)
-lexer ('(':cs) = TokenOP : lexer cs
-lexer (')':cs) = TokenCP : lexer cs
-lexer ('*':cs) = TokenCnj : lexer cs
-lexer ('+':cs) = TokenDsj : lexer cs
-lexer ('-':cs) = TokenNeg : lexer cs
-lexer ('=':'=':'>':cs) = TokenImpl : lexer cs
-lexer ('<':'=':'>':cs) = TokenEquiv : lexer cs
-lexer (x:_) = error ("unknown token: " ++ [x])
-
-lexNum cs = TokenInt (read num) : lexer rest
- where (num,rest) = span isDigit cs
-
-type Parser a b = [a] -> [(b,[a])]
-
-succeed :: b -> Parser a b
-succeed x xs = [(x,xs)]
-parseForm :: Parser Token Form
-parseForm (TokenInt x: tokens) = [(Prop x,tokens)]
-parseForm (TokenNeg : tokens) = [ (Neg f, rest) | (f,rest) <- parseForm tokens ]
-parseForm (TokenCnj : TokenOP : tokens) = [ (Cnj fs, rest) | (fs,rest) <- parseForms tokens ]
-parseForm (TokenDsj : TokenOP : tokens) = [ (Dsj fs, rest) | (fs,rest) <- parseForms tokens ]
-parseForm (TokenOP : tokens) = [ (Impl f1 f2, rest) | (f1,ys) <- parseForm tokens, (f2,rest) <- parseImpl ys ]
- ++
- [ (Equiv f1 f2, rest) | (f1,ys) <- parseForm tokens, (f2,rest) <- parseEquiv ys ]
-parseForm tokens = []
-
-parseForms :: Parser Token [Form]
-parseForms (TokenCP : tokens) = succeed [] tokens
-parseForms tokens =
- [(f:fs, rest) | (f,ys) <- parseForm tokens, (fs,rest) <- parseForms ys ]
-
-
-parseImpl :: Parser Token Form
-parseImpl (TokenImpl : tokens) =
- [ (f,ys) | (f,y:ys) <- parseForm tokens, y == TokenCP ]
-parseImpl tokens = []
-parseEquiv :: Parser Token Form
-parseEquiv (TokenEquiv : tokens) =
- [ (f,ys) | (f,y:ys) <- parseForm tokens, y == TokenCP ]
-parseEquiv tokens = []
-
-
-parse :: String -> [Form]
-parse s = [ f | (f,_) <- parseForm (lexer s) ]
-
-
-arrowfree :: Form -> Form 
-arrowfree (Prop x) = Prop x 
-arrowfree (Neg f) = Neg (arrowfree f)
-arrowfree (Cnj fs) = Cnj (map arrowfree fs)
-arrowfree (Dsj fs) = Dsj (map arrowfree fs)
-arrowfree (Impl f1 f2) = 
-  Dsj [Neg (arrowfree f1), arrowfree f2]
-arrowfree (Equiv f1 f2) = 
-  Dsj [Cnj [f1', f2'], Cnj [Neg f1', Neg f2']]
-  where f1' = arrowfree f1
-        f2' = arrowfree f2
-
-nnf :: Form -> Form 
-nnf (Prop x) = Prop x
-nnf (Neg (Prop x)) = Neg (Prop x)
-nnf (Neg (Neg f)) = nnf f
-nnf (Cnj fs) = Cnj (map nnf fs)
-nnf (Dsj fs) = Dsj (map nnf fs)
-nnf (Neg (Cnj fs)) = Dsj (map (nnf.Neg) fs)
-nnf (Neg (Dsj fs)) = Cnj (map (nnf.Neg) fs)
 
 ----------------------------------------------------------
 -- 1)
 
+
+contradiction :: Form -> Bool
+contradiction f = any (\ v -> not $ evl v f) (allVals f)
+
+tautology :: Form -> Bool
+tautology f = all (\ v -> evl v f) (allVals f)
+
+-- | logical entailment 
+entails :: Form -> Form -> Bool
+entails fp fq = all (\ v -> if evl v fp then evl v fq else True) (allVals fp) 
+
+-- | logical equivalence
+equiv :: Form -> Form -> Bool
+equiv fp fq = all (\ v ->  evl v fp == evl v fq) (allVals fp) 
+
+-- Examples from the workshop
+conCheck = contradiction $ Cnj [p, Neg p]
+
+tauCheck = contradiction $ Dsj [p, Neg p]
+
+entCheck = entails p $ Dsj [p, q]
+
+equCheck = equiv p p
+
+
+checkBatch1 :: IO()
+checkBatch1 = do
+    print("Contradiction: ")
+    if conCheck then print("Passed") else print("Failed")
+    print("Tautology: ")
+    if tauCheck then print("Passed") else print("Failed")
+    print("Entailment: ")
+    if entCheck then print("Passed") else print("Failed")
+    print("Equivalence: ")
+    if equCheck then print("Passed") else print("Failed")
+
+
+-- Result 1:
+-- main = checkBatch1
+
 ----------------------------------------------------------
 -- 2)
 
+-- To test parse I generate formulas from an [Int], this way parse can be tested with an arbitrary number of random formulas generated
+
+getRandomInt :: Int -> IO Int
+getRandomInt n = getStdRandom (randomR (0,n))
+
+getIntL :: Int -> Int -> IO [Int]
+getIntL _ 0 = return []
+getIntL k n = do 
+   x <-  getRandomInt k
+   xs <- getIntL k (n-1)
+   return (x:xs)
+
+genIntList :: Int -> Int -> IO [Int]
+genIntList kRange nRange = do 
+  k <- getRandomInt kRange
+  n <- getRandomInt nRange
+  getIntL k n
+
+
+-- Create an [Int], this will be the seed for the formula
+createSeed ::  IO [Int]
+createSeed = do
+    x <- getRandomInt 10
+    xs <- getIntL 3 (x)
+    return xs
+
+
+-- Turns a [Int] seed into a valid Form in String format ready to be processed by parse
+makeFormula :: [Int] -> String
+makeFormula [] =  show 0
+makeFormula [x] = show x
+makeFormula (x:xs)
+           | x == 0 = "-"++(makeFormula xs)  -- Neg p
+           | x == 1 = "+("++(makeFormula xs)++" "++(show $ head xs)++")"  -- Disjunction p q
+           | x == 2 = "*("++(makeFormula xs)++" "++(show $ head xs)++")"  -- Conjunction p q                      -- 
+           | x == 3 = "("++(makeFormula xs)++"==>"++(show $ head xs)++")" -- Entailment
+           | x == 4 = "("++(makeFormula xs)++"<=>"++(show $ head xs)++")" -- Equivalence
+           | otherwise = ""
+           
+
+-- Just to see if makeFormula works as expected
+checkFormula :: IO()
+checkFormula = do
+    x <- createSeed
+    print (makeFormula x)
+    print (makeFormula x)
+    print (makeFormula x)
+
+
+-- Limit quickCheck to a list of numbers between 0 and 4 ()
+genPos :: Gen Int
+genPos = abs `fmap` (arbitrary :: Gen Int) `suchThat` (\x -> (x >= 0 && x < 5))
+
+genListOfPos :: Gen [Int]
+genListOfPos = listOf genPos
+
+-- parse works as expected if for any random, valid formula the output will only contain one element, the correctly parsed formula 
+parseProperty :: [Int] -> Property
+parseProperty xs = length xs < 30 ==> length (parse (makeFormula xs)) == 1
+
+checkParse :: IO()
+checkParse = do
+    quickCheck $ forAll genListOfPos $ parseProperty
+
+-- Result 2:
+-- main = checkParse
 
 ----------------------------------------------------------
 -- 3)
 
-toCNF :: Form -> Form
+-- Check if arrowfree fulfills the postconditions
+
+arrowProperty :: [Int] -> Property
+arrowProperty xs = length xs < 30 ==> not(elem '=' $ (show . arrowfree . head . parse . makeFormula) xs) && equiv ((head . parse . makeFormula) xs) ((arrowfree . head . parse . makeFormula) xs )
+
+checkArrowfree :: IO()
+checkArrowfree = do
+    quickCheck $ forAll genListOfPos $ arrowProperty
+
+-- Check if nnf fulfills the postconditions
+nnfProperty :: [Int] -> Property
+nnfProperty xs = length xs < 30 ==> not(isInfixOf "-(" $ (show . nnf . arrowfree . head . parse . makeFormula) xs) 
+
+checkNnf :: IO()
+checkNnf = do
+    quickCheck $ forAll genListOfPos $ nnfProperty
+
+toCNF :: Form ->  Form
 toCNF f = (nnf . arrowfree) f
 
 stringToCNF :: String -> Form
 stringToCNF s = (toCNF . head . parse) s
 
 
-
-
 ----------------------------------------------------------
 -- 4)
 
 
-
-
-
-main = print(stringToCNF "(1 <=> +(2 -3))")
+main = checkNnf
