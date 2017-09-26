ae2783bf37d574c92a622ad24c3adbd2b3172c2d
Herczeg <h.herczeg.andras@gmail.com>
2017-09-26 11:29:18 +0200

    Added partial solution for exercise 3

--- andras/Week 4/Assignment4.hs
+++ andras/Week 4/Assignment4.hs
@@ -11,13 +11,7 @@ genLimit = choose (-1000, 1000)
 genListOfLimit :: Gen [Int]
 genListOfLimit = listOf genLimit
 
--- To calculate the lenght we use the fact that a set will only be a subset of its first n elements if n == length(set) 
-setLength :: Ord a => Set a -> Int
-setLength s = (setLengthN s 0) 
 
-setLengthN :: Ord a => Set a -> Int -> Int
-setLengthN s n = if subSet s (takeSet n s) then n else setLengthN s (n+1) 
---setLengthN s 10000 = -1 -- Set a limit just in case of an infinite recursion
 
 
 ------------------------------------------------------------------
@@ -26,7 +20,7 @@ setLengthN s n = if subSet s (takeSet n s) then n else setLengthN s (n+1)
 
 
 ------------------------------------------------------------------
--- 2)
+-- 2) Time spent ~ 45 minutes
 
 getRandomInt :: Int -> IO Int
 getRandomInt n = getStdRandom (randomR (0,n))
@@ -80,24 +74,46 @@ runDataGen2 = do
     quickCheck (\x -> setProperty1 (seedToSet x))
     quickCheck (\x -> setProperty2 (seedToSet x))
 
+-- Result 2:
+-- main = runDataGen1
+-- main = runDataGen2
 
 ------------------------------------------------------------------
 -- 3)
 
-{-
-unionSet :: (Ord a) => Set a -> Set a -> Set a 
-unionSet (Set [])     set2  =  set2
-unionSet (Set (x:xs)) set2  = 
-   insertSet x (unionSet (Set xs) set2)
--}
+
+-- To calculate the lenght we use the fact that a set will only be a subset of its first n elements if n == length(set) 
+setLength :: Ord a => Set a -> Int
+setLength s = (setLengthN s 0) 
+
+setLengthN :: Ord a => Set a -> Int -> Int
+setLengthN s n = if subSet s (takeSet n s) then n else setLengthN s (n+1) 
+--setLengthN s 10000 = -1 -- Set a limit just in case of an infinite recursion
+
 
 intersectSet :: (Ord a) => Set a -> Set a -> Set a 
 intersectSet (Set [])     set2  =  (Set [])
+intersectSet set1 set2  =  intersectSetN set1 set2 (Set []) (setLength set1)
+
+-- We check each element of set1 if they are an element of set2, if yes, we add them to set3. When we parsed through all the elements of set1 we return set3 
+intersectSetN :: (Ord a) => Set a -> Set a -> Set a -> Int -> Set a
+intersectSetN set1 set2 set3 0 = set3
+intersectSetN (Set (x:xs)) set2 set3 n = if inSet x set2 then intersectSetN (Set (xs)) set2 (insertSet x set3) (n-1) else intersectSetN (Set (xs)) set2 set3 (n-1) 
 
 
+-- A difference set of set1 and set2 is the same as (set1 ∨ set2) - (set1 ∧ set2)
 differenceSet :: (Ord a) => Set a -> Set a -> Set a 
 differenceSet (Set [])     set2  =  set2
+differenceSet set1 set2 = differenceSetN (unionSet set1 set2) (intersectSet set1 set2) (Set []) (setLength(unionSet set1 set2))
 
+-- We check each element of set1 if they are an element of set2, if no, we add them to set3. When we parsed through all the elements of set1 we return set3
+differenceSetN ::  (Ord a) => Set a -> Set a -> Set a -> Int -> Set a
+differenceSetN set1 set2 set3 0 = set3
+differenceSetN (Set (x:xs)) set2 set3 n = if not(inSet x set2) then differenceSetN (Set (xs)) set2 (insertSet x set3) (n-1) else differenceSetN (Set (xs)) set2 set3 (n-1) 
+
+
+--setOperationProperty1 :: Ord a =>  Set a -> Set a -> Bool
+--setOperationProperty1
 
 ------------------------------------------------------------------
 -- 4)
@@ -130,5 +146,4 @@ r @@ s =
 
 
 
-main = runDataGen2
-
+main = print(differenceSet (Set [1, 2, 3, 4]) (Set [1, 2, 5, 6]))
