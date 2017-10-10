41c8126f2d5729cb32c4c40e2f79a177ad7b1908
Metchu <ahmednassir@hotmail.com>
2017-10-10 16:42:45 +0200

    added spent time

--- _group/week6/Lab6.hs
+++ _group/week6/Lab6.hs
@@ -267,7 +267,9 @@ main2 = do
 
 ----------------------------------------
 
--- Exercise 3
+{-
+Exercise 3 (~5 minutes)
+-}
 
 -- Composites are numbers that are greater than 1 and not primes 
 -- (1 is not a composite because it only has one factor)
@@ -276,7 +278,9 @@ composites = filter (not.prime) [2..]
 
 ----------------------------------------
 
--- Exercise 4
+{-
+Exercise 4 (~60 minutes)
+-}
 
 -- 1st argument is the primality check to be applied to each number of the list in the 3rd argument
 testPrimes :: (Int -> Integer -> IO Bool) -> Int -> [Integer] -> IO ()
@@ -296,7 +300,9 @@ main4 = do testPrimes primeTestsF 1  composites
 
 -----------------------------------------
 
--- Exercise 5
+{-
+Exercise 5 (~30 minutes)
+-}
 
 carmichael :: [Integer]
 carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) | 
@@ -323,16 +329,19 @@ main5 = do testPrimes primeTestsF 1  carmichael
 
 ------------------------------------------
 
--- Exercise 6
+{-
+Exercise 6a (~15 minutes)
+Exercise 6b (~45 minutes)
+-}
 
 -- It seems the Miller-Rabin primality check is not weak against Carmichael numbers.
 --    Even for K == 1 the test returns 'Composite' for the first 5 Carmichael numbers the majority of times.
 
-main6 = do testPrimes primeMR 1  carmichael
-           testPrimes primeMR 2  carmichael
-           testPrimes primeMR 3  carmichael
-           -- For k > 3 it takes a lot of time to process
-           print (take 5 carmichael)
+main6a = do testPrimes primeMR 1  carmichael
+            testPrimes primeMR 2  carmichael
+            testPrimes primeMR 3  carmichael
+            -- For k > 3 it takes a lot of time to process
+            print (take 5 carmichael)
 
 largeMRPrime' :: [Integer] -> IO Int
 largeMRPrime' (x:xs) = do isPrime <- primeMR 2 (2^x - 1)
@@ -355,7 +364,7 @@ largeMRPrime n = do p <- largeMRPrime' [x | x <- primes, x > toInteger n]
 -- 446087557183758429571151706402101809886208632412859901111991219963404685792820473369112545269003989026153245931124316702395758705693679364790903497461147071065254193353938124978226307947312410798874869040070279328428810311754844108094878252494866760969586998128982645877596028979171536962503068429617331702184750324583009171832104916050157628886606372145501702225925125224076829605427173573964812995250569412480720738476855293681666712844831190877620606786663862190240118570736831901886479225810414714078935386562497968178729127629594924411960961386713946279899275006954917139758796061223803393537381034666494402951052059047968693255388647930440925104186817009640171764133172418132836351
 -- Mersenne prime: yes (exponent = 2281)
 
-main62 = do n <- getRandomInt 1000
+main6b = do n <- getRandomInt 1000
             p <- largeMRPrime n
             print (2^p - 1)
             p2 <- largeMRPrime p
@@ -365,6 +374,8 @@ main62 = do n <- getRandomInt 1000
 
 -------------------------------------------------------
 
--- Exercise 7
+{-
+Exercise 7 (~ minutes)
+-}
 
 main = print "exercise7"
